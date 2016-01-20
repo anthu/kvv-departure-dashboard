c5018676ba97b0cef358656030dc54920e6f79aa
Anton Huck <antonhuck@gmail.com>
2016-01-20 11:54:06 +0100

    refactoring

---
+++ jobs/kvv.rb
@@ -0,0 +1,29 @@
+require 'net/http'
+require 'json'
+
+def reformat_time(time_str)
+  if time_str == "0"
+    time_str = "sofort"
+  end
+  return time_str
+end
+
+def get_update_item(stop_id)
+  logger = Logger.new(STDOUT)
+  logger.level = Logger::DEBUG
+  parsed_request = kvv_request(stop_id)
+  departures = parsed_request["departures"]
+  departures.map { |departure|
+    departure["time"] = reformat_time(departure["time"])
+  }
+
+  return { items: departures, stop: parsed_request["stopName"] }
+end
+
+
+def kvv_request(stop_id)
+  uri = URI.parse("http://live.kvv.de/webapp/departures/bystop/" + stop_id + "?key=377d840e54b59adbe53608ba1aad70e8&maxInfos=5")
+  resp = Net::HTTP.get(uri)
+  parsed = JSON.parse(resp)
+  return parsed
+end
--- jobs/kvv_auerstr.rb
+++ jobs/kvv_auerstr.rb
@@ -1,37 +1,9 @@
-require 'net/http'
-require 'json'
+require_relative 'kvv'
 
 SCHEDULER.every '20s', :first_in => 0 do
-  auerstr = "de:8212:11"
+  auerstr_id = "de:8212:11"
 
-  data_auerstr = get_update_item(auerstr)
+  data_auerstr = get_update_item(auerstr_id)
 
   send_event('departures_auerstr', data_auerstr)
-end
-
-def reformat_time(time_str)
-  if time_str == "0"
-    time_str = "sofort"
-  end
-  return time_str
-end
-
-def get_update_item(stop_id)
-  logger = Logger.new(STDOUT)
-  logger.level = Logger::DEBUG
-  parsed_request = kvv_request(stop_id)
-  departures = parsed_request["departures"]
-  departures.map { |departure|
-    departure["time"] = reformat_time(departure["time"])
-  }
-
-  return { items: departures, stop: parsed_request["stopName"] }
-end
-
-
-def kvv_request(stop_id)
-  uri = URI.parse("http://live.kvv.de/webapp/departures/bystop/" + stop_id + "?key=377d840e54b59adbe53608ba1aad70e8&maxInfos=5")
-  resp = Net::HTTP.get(uri)
-  parsed = JSON.parse(resp)
-  return parsed
 end
